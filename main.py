0d317a4bb603c00538de5a85f38e30d799afd6f3
Egor Oparin <uegorigor@gmail.com>
2023-07-22 13:25:27 +0300

    ширина docbar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize
 
 class MainWindow(QWidget):
     def __init__(self, title):
@@ -17,19 +17,33 @@ class MainWindow(QWidget):
 
     def createWidgets(self):
         self.dockbar = QMenuBar()
+        self.dockbar.setMaximumSize(QSize(1920, 25))
+
         self.fileButton = QMenu("File")
         self.settingsButton = QMenu("Settings")
         self.dockbar.addMenu(self.fileButton)
         self.dockbar.addMenu(self.settingsButton)
 
+        self.openFileAction = QAction('open')
+        self.saveFileAction = QAction('save')
+        self.fileButton.addActions([self.openFileAction, self.saveFileAction])
+
+        self.picLabel = QLabel('#место для картинки') 
+
     def initUI(self):
         self.createLayout()
         self.createWidgets()
         self.setLayout(self.mainLine)
+
         self.mainLine.addLayout(self.dockbarLine)
-        self.dockbarLine.addWidget(self.dockbar)
+        self.mainLine.addLayout(self.centerLine)
+
+        self.centerLine.addWidget(self.picLabel)
+        self.centerLine.addLayout(self.toolsLine)
+
+        self.dockbarLine.addWidget(self.dockbar, alignment=Qt.AlignTop)
 
 
 main = QApplication([])
-mainW = MainWindow('window')
+mainW = MainWindow('ImageEditor')
 main.exec_()
