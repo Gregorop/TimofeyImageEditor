14ba4a0b6b497a813816887412ab80afc4c6612b
Egor Oparin <uegorigor@gmail.com>
2023-07-22 13:39:08 +0300

    загрушка картинки

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QWidget):
     def __init__(self, title):
         super().__init__()
         self.setWindowTitle(title)
-        self.showMaximized()
+        
         self.initUI()
+        self.showMaximized()
         self.show()
 
     def createLayout(self):
         self.mainLine = QVBoxLayout()
         self.dockbarLine = QHBoxLayout()
-        self.centerLine = QHBoxLayout()
-        self.toolsLine = QVBoxLayout()
+        self.toolsLine = QHBoxLayout() #теперь это будет панелька под docбаром
 
     def createWidgets(self):
         self.dockbar = QMenuBar()
@@ -29,6 +30,10 @@ class MainWindow(QWidget):
         self.fileButton.addActions([self.openFileAction, self.saveFileAction])
 
         self.picLabel = QLabel('#место для картинки') 
+        self.pixmap = QPixmap('blank.png') #картинка как набор пикселей, для рисования QPainter
+        '''https://www.riverbankcomputing.com/static/Docs/PyQt5/api/qtgui/qpainter.html?highlight=qpainter#QPainter'''
+
+        self.picLabel.setPixmap(self.pixmap)
 
     def initUI(self):
         self.createLayout()
@@ -36,12 +41,12 @@ class MainWindow(QWidget):
         self.setLayout(self.mainLine)
 
         self.mainLine.addLayout(self.dockbarLine)
-        self.mainLine.addLayout(self.centerLine)
 
-        self.centerLine.addWidget(self.picLabel)
-        self.centerLine.addLayout(self.toolsLine)
-
+        #? почему между докбаром и картинкой километр пустоты?
         self.dockbarLine.addWidget(self.dockbar, alignment=Qt.AlignTop)
+        self.mainLine.addWidget(self.picLabel, alignment=Qt.AlignCenter)
+
+        
 
 
 main = QApplication([])
